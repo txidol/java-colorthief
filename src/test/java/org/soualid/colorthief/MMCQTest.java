e17801d1809599eb9f52316e382d2a9d907ba73b
Simon Oualid <simon.oualid@gmail.com>
2014-01-28 02:37:28 +0100

    documentation update

--- src/test/java/org/soualid/colorthief/MMCQTest.java
+++ src/test/java/org/soualid/colorthief/MMCQTest.java
@@ -16,7 +16,13 @@ public class MMCQTest extends TestCase {
 		List<int[]> result = MMCQ.compute(img, 10);
 		for (int i=0; i<result.size(); i++) {
 			int[] is = result.get(i);
-			System.out.println("rgb: " + is[0] + ", " + is[1] + ", " + is[2]);
+			System.out.println(is[0] + ", " + is[1] + ", " + is[2]);
+			/*
+			 * output is RGB values: 
+			 * 214, 217, 215
+			 * 203, 123, 131
+			 * ...
+			 */
 		}
 	}
 }
